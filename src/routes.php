3b6d9463a6db2fa9be274f26c0db692eeeb2b70d
Jacob Wunder <jacob@jacobwunder.com>
2017-11-19 16:10:45 -0500

    now serving the static frontend!

--- src/routes.php
+++ src/routes.php
@@ -4,25 +4,21 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\Stream;
 
-// use Models\User;
-
-
 // Routes
+$app->get('/', function (Request $request, Response $response, array $args) {
+  $mimes = new \Mimey\MimeTypes;
+  $path = __DIR__ . '/../front/build/index.html';
+  $file = fopen($path, 'rb');
+  $type = $mimes->getMimeType(end(explode('.', $path)));
 
-// $app->get('/api/user', function (Request $request, Response $response, array $args) {
-//
-//   return $response->withJson(var_dump($this));
-//
-//   return $response->withJson(array('name' => 'jacob', 'age' => 19));
-//
-// });
+  return $response
+    ->withHeader('Content-Type', $type)
+    ->withBody(new Stream($file));
+});
 
-$app->get('/{path:.*}', function (Request $request, Response $response, array $args) {
+$app->get('/front/{path:.*}', function (Request $request, Response $response, array $args) {
   $users = $this->db::table('users');
-
-  // Sample log message
-  // $this->logger->info(__DIR__ . '/../front/build/favicon.png');
-  $this->logger->info('serving ' . $args['path'] . ' yup');
+  $mimes = new \Mimey\MimeTypes;
 
   // $this->db::table('users')->insert(['netid' => $args['name']]);
   // return $response->withJson(var_dump( $users->get() ));
@@ -30,16 +26,27 @@ $app->get('/{path:.*}', function (Request $request, Response $response, array $a
 
   // TODO: check if the string has a "/../" in it
 
-  if (!isset($args['path']))
-    $args['path'] = 'index.html';
+  $this->logger->info('serving :' . $args['path']);
+
+  if ($args['path'] == '/') $args['path'] = 'index.html';
 
   $path = __DIR__ . '/../front/build/' . $args['path'];
   $file = fopen($path, 'rb');
+  $type = $mimes->getMimeType(end(explode('.', $path)));
 
   return $response
-    ->withHeader('Content-Type', mime_content_type($file))
+    ->withHeader('Content-Type', $type)
     ->withBody(new Stream($file));
 
   // Render index view
   // return $this->renderer->render($response, $args['path'], $args);
+});
+
+
+$app->get('/api/user', function (Request $request, Response $response, array $args) {
+
+  // return $response->withJson(var_dump($this));
+
+  return $response->withJson(array('name' => 'jacob', 'age' => 19));
+
 });
